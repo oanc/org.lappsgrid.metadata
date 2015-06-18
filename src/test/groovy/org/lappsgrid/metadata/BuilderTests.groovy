aafd707f6b9cd6073286f87cf85fe19ff7b2a304
Keith Suderman <suderman@anc.org>
2015-06-18 16:47:06 -0400

    Added buider class for ServiceMetadata.

---
+++ src/test/groovy/org/lappsgrid/metadata/BuilderTests.groovy
@@ -0,0 +1,39 @@
+package org.lappsgrid.metadata
+
+import static org.junit.Assert.*
+import org.junit.Test
+import static org.lappsgrid.discriminator.Discriminators.Uri
+
+/**
+ * @author Keith Suderman
+ */
+class BuilderTests {
+
+    @Test
+    void testTokenizerSplitter() {
+        ServiceMetadata tokenizer = new ServiceMetadataBuilder()
+            .allow(Uri.ANY)
+            .name("GATE Tokenizer")
+            .vendor("http://www.anc.org")
+            .version("2.0.0")
+            .requireEncoding("UTF-8")
+            .produceEncoding("UTF-8")
+            .requireFormat(Uri.TEXT)
+            .requireFormat(Uri.XML)
+            .requireFormat(Uri.GATE)
+            .produceFormat(Uri.GATE)
+            .produce(Uri.TOKEN)
+            .build();
+        ServiceMetadata splitter = new ServiceMetadataBuilder()
+            .allow(Uri.ANY)
+            .name("GATE SentenceSplitter")
+            .vendor("http://www.anc.org")
+            .version("2.0.0")
+            .requireFormat(Uri.GATE)
+            .produceFormat(Uri.GATE)
+            .require(Uri.TOKEN)
+            .produce(Uri.SENTENCE).build();
+
+        assertTrue tokenizer.produces.satisfies(splitter.requires)
+    }
+}
