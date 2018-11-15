c6921da39630ea529cabc4bb125815e07025cbf5
Keith Suderman <suderman@cs.vassar.edu>
2018-11-15 18:20:53 -0500

    Update URL to metadata schema

--- src/main/groovy/org/lappsgrid/metadata/ServiceMetadata.groovy
+++ src/main/groovy/org/lappsgrid/metadata/ServiceMetadata.groovy
@@ -40,7 +40,7 @@ import org.lappsgrid.serialization.Data
 @JsonPropertyOrder(["schema","name","version","description","vendor","allow","license", "licenseDesc","url", "parameters", "requires", "produces"])
 class  ServiceMetadata {
 
-    public static final String DEFAULT_SCHEMA_URL = 'http://vocab.lappsgrid.org/schema/service-schema-1.0.0.json'
+    public static final String DEFAULT_SCHEMA_URL = 'https://vocab.lappsgrid.org/schema/1.1.0/metadata-schema.json'
 
     /** The JSON schema that describes the JSON format. */
     @JsonProperty('$schema')
